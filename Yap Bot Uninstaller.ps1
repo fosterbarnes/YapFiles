a9a4314d78711f8579f2614bfe774b75f3bc04f0
fosterbarnes <152516585+fosterbarnes@users.noreply.github.com>
2024-08-23 17:40:31 -0400

    Add files via upload

---
+++ Yap Bot Uninstaller.ps1
@@ -0,0 +1,22 @@
+﻿# Define file and folder paths
+$yapBotFolderPath = "$env:USERPROFILE\Documents\Applications\Yap Bot"
+$desktopShortcutPath = "$env:USERPROFILE\Desktop\Twitch Yap Bot.lnk"
+$startMenuShortcutPath = "$env:USERPROFILE\Documents\Pinned Folders\Start Menu Programs\Twitch Yap Bot.lnk"
+
+# Check and remove Yap Bot folder if it exists
+if (Test-Path -Path $yapBotFolderPath) {
+    Remove-Item -Path $yapBotFolderPath -Recurse -Force -Confirm:$false
+} else {
+}
+
+# Check and remove desktop shortcut if it exists
+if (Test-Path -Path $desktopShortcutPath) {
+    Remove-Item -Path $desktopShortcutPath -Force -Confirm:$false
+} else {
+}
+
+# Check and remove start menu shortcut if it exists
+if (Test-Path -Path $startMenuShortcutPath) {
+    Remove-Item -Path $startMenuShortcutPath -Force -Confirm:$false
+} else {
+}
