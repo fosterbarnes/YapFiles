5d8e33ba793f081f7de65d6463eddc4ffd3f92b2
fosterbarnes <152516585+fosterbarnes@users.noreply.github.com>
2024-10-08 02:54:55 -0400

    updated to remove "Yap Updater" shortcuts

--- YapBotUninstaller.ps1
+++ YapBotUninstaller.ps1
@@ -2,8 +2,10 @@
 $yapBotFolderPath = "$env:USERPROFILE\Documents\Applications\Yap Bot"
 $desktopShortcutPath = "$env:USERPROFILE\Desktop\Twitch Yap Bot.lnk"
 $desktopShortcutPath2 = "$env:USERPROFILE\Desktop\Yap Editor.lnk"
+$desktopShortcutPath3 = "$env:USERPROFILE\Desktop\Yap Updater.lnk"
 $startMenuShortcutPath = "$env:USERPROFILE\Documents\Pinned Folders\Start Menu Programs\Twitch Yap Bot.lnk"
 $startMenuShortcutPath2 = "$env:USERPROFILE\Documents\Pinned Folders\Start Menu Programs\Yap Editor.lnk"
+$startMenuShortcutPath3 = "$env:USERPROFILE\Documents\Pinned Folders\Start Menu Programs\Yap Updater.lnk"
 
 # Check and remove Yap Bot folder if it exists
 if (Test-Path -Path $yapBotFolderPath) {
@@ -23,6 +25,12 @@ if (Test-Path -Path $desktopShortcutPath2) {
 } else {
 }
 
+# Check and remove desktop shortcut if it exists
+if (Test-Path -Path $desktopShortcutPath3) {
+    Remove-Item -Path $desktopShortcutPath3 -Force -Confirm:$false
+} else {
+}
+
 # Check and remove start menu shortcut if it exists
 if (Test-Path -Path $startMenuShortcutPath) {
     Remove-Item -Path $startMenuShortcutPath -Force -Confirm:$false
@@ -32,6 +40,12 @@ if (Test-Path -Path $startMenuShortcutPath) {
 # Check and remove start menu shortcut if it exists
 if (Test-Path -Path $startMenuShortcutPath2) {
     Remove-Item -Path $startMenuShortcutPath2 -Force -Confirm:$false
+} else {
+}
+
+# Check and remove start menu shortcut if it exists
+if (Test-Path -Path $startMenuShortcutPath3) {
+    Remove-Item -Path $startMenuShortcutPath3 -Force -Confirm:$false
 } else {
 }
 
